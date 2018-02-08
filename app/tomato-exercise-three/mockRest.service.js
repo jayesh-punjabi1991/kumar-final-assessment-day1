0cd049ea6cb2c3b4a639d71b9a1433d1c1fd7a4f
Manoj Kumar <Manoj.Kumar@mea.ey.net>
2018-02-08 13:11:05 +0530

    Day3 changes

--- app/tomato-exercise-three/mockRest.service.js
+++ app/tomato-exercise-three/mockRest.service.js
@@ -10,7 +10,8 @@
 	function mockRestService($http, $q) {
 
 		var service = {
-            getListItems: getListItems
+			getListItems: getListItems,
+			designQueryParams: designQueryParams
 		};
 
 		return service;
@@ -18,13 +19,35 @@
 		function getListItems(listTitle, queryParams) {
             var dfd = $q.defer();
             $http.defaults.headers.post['X-HTTP-Method'] = "";
-            var restUrl = "../_api/web/lists/getbytitle('" + listTitle + "')/items" + queryParams;
+            var restUrl = "../_api/web/lists/getbytitle('" + listTitle + "')/items?" + designQueryParams(queryParams).join("&");
             $http.get(restUrl).success(function(data) {
                 dfd.resolve(data.d.results);
             }).error(function(data) {
                 dfd.reject("error, cannot get items"); 
             });
             return dfd.promise;
+		}
+
+		function designQueryParams(params) {
+			var queryObjects = [];
+
+			if (params.filter) {
+				queryObjects.push("$filter=" + params.filter);
+			}
+
+			if (params.expand) {
+				queryObjects.push("$expand=" + params.expand);
+			}
+
+			if (params.select) {
+				queryObjects.push("$select=" + params.select);
+			}
+
+			if (params.top) {
+				queryObjects.push("$top=" + params.top);
+			}
+
+			return queryObjects;
 		}
 
 	}
@@ -54,49 +77,27 @@
 	}
 */
 
-/*************** INSERT ANSWERS BELOW IN THE COMMENTED AREA**************
- 
+/*************** INSERT ANSWERS BELOW IN THE COMMENTED AREA**************/
+function getWorkPlanItems() {
+	var queryParams = {
+		select: "Id,Owner/Title,Function/Title,WorkPlanTaskType,WorkPlanTaskStatus/Title",
+		expand: "Owner,Function,WorkPlanTaskStatus",
+		orderby: "TaskDueDate",
+		top: 2000
+	};
+	mockRestService.getListItems('WorkPlan', queryParams).then(function(response) {
+		return response;
+	})
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- */
+function getRisksAndIssuesItems() {
+	var queryParams = {
+		select: "Id,Title,Function/Title,Description,ItemOwner/Title",
+		expand: "Function,ItemOwner",
+		filter: "RiskImpact eq 'High'",
+		top: 500
+	};
+	mockRestService.getListItems('Risks and Issues', queryParams).then(function(response) {
+		return response;
+	})
+}
--- app/tomato-exercise-three/tomato-exercise-three.controller.js
+++ app/tomato-exercise-three/tomato-exercise-three.controller.js
@@ -11,6 +11,8 @@
 
     function TomatoExerciseThreeController($scope, restService, $q) {
         var vm = this;
+        vm.ordersByCountryDetails = ordersByCountryDetails;
+        vm.totalTomatoesOrderedByCountry = totalTomatoesOrderedByCountry;
 
         activate();
 
@@ -21,6 +23,11 @@
 
             function activateComplete(results) {
                 vm.data = results[0];
+                vm.ordersByCountries = ordersByCountryDetails(vm.data);
+                console.log('FORMAT ONE: ' + vm.ordersByCountries);
+                
+                vm.tomatoesByCountry = totalTomatoesOrderedByCountry(vm.data);
+                console.log('FORMAT TWO: ' + vm.tomatoesByCountry);
             }
         }
 
@@ -31,7 +38,30 @@
         }
 
         //Add your code below.
+        function ordersByCountryDetails(responseData) {
+            var aggregateData = _(responseData[3].orders).groupBy('Countries.ID').map(function(item, itemId) {
+                var obj = [];
+                var countryName = _(responseData[0].countries).filter({ID: parseInt(itemId)}).value()[0].Title;
+                _.forEach(item, function(element) {
+                    element['Countries'] = _(responseData[0].countries).filter({ID: parseInt(itemId)}).value();
+                    element['Tomato'] = _(responseData[1].tomatoes).filter({ID: parseInt(element.Tomato.ID)}).value();
+                    element['Status'] = _(responseData[2].statuses).filter({ID: parseInt(element.Status.ID)}).value();
+                });
+                obj.push(countryName, item);
+                return obj;
+            }).fromPairs().value();
+            return aggregateData;
+        }
 
+        function totalTomatoesOrderedByCountry(responseData) {
+            var aggregateData = _(responseData[3].orders).groupBy('Countries.ID').map(function(item, itemId) {
+                var obj = [];
+                var countryName = _(responseData[0].countries).filter({ID: parseInt(itemId)}).value()[0].Title;
+                obj.push(countryName, _(item).reduce(function(sum, n) { return sum + parseInt(n.Qty)}, 0));
+                return obj;
+            }).fromPairs().value();
+            return aggregateData;
+        }
 
     }
 })();
